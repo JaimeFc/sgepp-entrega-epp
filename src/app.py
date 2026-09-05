05b02df7de9047cbd6ace85f0b0f7428ac34f2db
JaimeFc <159406959+JaimeFc@users.noreply.github.com>
2026-09-04 23:02:16 -0500

    Agrega ruta inicial para gestion de entregas EPP

--- src/app.py
+++ src/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def inicio():
     return "Modulo de Registro y Control de Entrega de EPP"
 
+@app.route('/entregas')
+def entregas():
+    return "Modulo de entregas de EPP"
+
 if __name__ == '__main__':
     app.run(debug=True)
